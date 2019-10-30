d7a3b2820302029dece6b39084f164f272fab028
byron baldwin <byronsbaldwin@gmail.com>
2019-10-30 09:53:21 -0500

    No.1 DOM

---
+++ DOM/script.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', function() {
+    let btn = document.createElement('button');
+    btn.className = 'button1';
+    let button1 = document.getElementsByClassName('button');
+    btn.class = button1;
+    document.body.appendChild(btn);
+});
