fcd8b5a67e4f6cedc3fc32038dc64bab49452a6b
byron baldwin <byronsbaldwin@gmail.com>
2019-11-04 13:00:24 -0600

    No.2

--- DOM/script.js
+++ DOM/script.js
@@ -3,5 +3,23 @@ window.addEventListener('DOMContentLoaded', function() {
     btn.className = 'button1';
     let button1 = document.getElementsByClassName('button');
     btn.class = button1;
+    let buttonText = document.createTextNode('Click Here');
+    btn.appendChild(buttonText);
+
+    btn.addEventListener('click',function() {
+        alert('Hello, I hope you are doing well today');
+    })
+
+    document.getElementById('btn2').addEventListener('click', function() {
+        alert(document.getElementById('message').value);
+    });
+
+    let div = document.createElement('div');
+    div.className = 'div1';
+    let divOne = document.getElementsByClassName('div');
+    div.class = divOne;
+    document.body.appendChild(div);
+
     document.body.appendChild(btn);
+
 });
