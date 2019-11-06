a73681f2469f70d43ddd6340fcc6a800cc5be415
byron baldwin <byronsbaldwin@gmail.com>
2019-11-05 20:22:32 -0600

    No.3

--- DOM/script.js
+++ DOM/script.js
@@ -5,40 +5,25 @@ window.addEventListener('DOMContentLoaded', function() {
     btn.class = button1;
     let buttonText = document.createTextNode('Click Here');
     btn.appendChild(buttonText);
+    document.body.appendChild(btn);
 
     btn.addEventListener('click',function() {
         alert('Hello, I hope you are doing well today');
-    })
+    });
 
     document.getElementById('btn2').addEventListener('click', function() {
         alert(document.getElementById('message').value);
     });
 
-    let div = document.createElement('div');
-    div.className = 'div1';
-    let divOne = document.getElementsByClassName('div');
-    div.class = divOne;
-    let h2 = document.createElement('h2');
-    h2.className = 'header2';
-    let header2 = document.getElementsByClassName('h2');
-    h2.class = header2;
-    let h2Text = document.createTextNode('This should change color when the mouse hovers over');
-    h2.appendChild(h2Text);
-    div.appendChild(h2);
-    document.body.appendChild(div);
-
-    
-    div.addEventListener('mouseover', function() {
-        div.style.backgroundcolor = 'green';
+    document.getElementById('message').addEventListener('mouseover', function(e) {
+        e.target.style.backgroundColor = 'yellow';
     });
-    
+
+    document.getElementById('message').addEventListener('mouseout', function(e) {
+        e.target.style.backgroundColor = '';
+    });
 
     
-
-
-
-
-    document.body.appendChild(btn);
 
 });
 
