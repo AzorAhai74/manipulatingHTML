8612ee411a07860e746b3ada745adcb41714d6dd
byron baldwin <byronsbaldwin@gmail.com>
2019-11-13 17:18:32 -0600

    No.4 Red

--- JQuery/app.js
+++ JQuery/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    let $button1 = $("<button></button>");
+    let $button1 = $('<button></button>');
     let $btn1Text = 'Click Here';
     
     $($button1).append($btn1Text);
@@ -25,4 +25,20 @@ $(document).ready(function() {
             'background-color': ''
         });
     });
+    
+    let $paraOne = $('<p>');
+    let $paraText = 'This is the text in a paragragh';
+
+    $($paraOne).append($paraText);
+    $('body').append($paraOne);
+
+    function clickPara () {
+        $('p').on('click', function() {
+            $(this).css({
+                'color': 'red'
+            });
+        });
+    };
+    clickPara();
+    
 });
